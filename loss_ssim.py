ed7f69ae14bc6138afa52425ab2c62bae1e87237
QingXIA233 <64100948+QingXIA233@users.noreply.github.com>
2020-08-16 09:36:12 +0200

    Add files via upload

---
+++ loss_ssim.py
@@ -0,0 +1,101 @@
+"""Contains losses used for performing image-to-image domain adaptation."""
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+def depth_loss_function(y_true, y_pred):
+
+    theta=0.1
+    maxDepthVal=1000.0/10.0
+    
+    # Point-wise depth
+    l_depth = K.mean(K.abs(y_pred - y_true), axis=-1)
+
+    # Edges
+    dy_true, dx_true = tf.image.image_gradients(y_true)
+    dy_pred, dx_pred = tf.image.image_gradients(y_pred)
+    l_edges = K.mean(K.abs(dy_pred - dy_true) + K.abs(dx_pred - dx_true), axis=-1)
+
+    # Structural similarity (SSIM) index
+    l_ssim = K.clip((1 - tf.image.ssim(y_true, y_pred, maxDepthVal)) * 0.5, 0, 1)
+
+    # Weights
+    w1 = 1.0
+    w2 = 1.0
+    w3 = theta
+
+    return (w1 * l_ssim) + (w2 * K.mean(l_edges)) + (w3 * K.mean(l_depth))
+
+def cycle_consistency_ssim_loss(real_images, generated_images):
+
+    ssim_loss = depth_loss_function(real_images, generated_images)
+
+    return ssim_loss
+
+
+def cycle_consistency_loss(real_images, generated_images):
+    """Compute the cycle consistency loss.
+
+    The cycle consistency loss is defined as the sum of the L1 distances
+    between the real images from each domain and their generated (fake)
+    counterparts.
+
+    This definition is derived from Equation 2 in:
+        Unpaired Image-to-Image Translation using Cycle-Consistent Adversarial
+        Networks.
+        Jun-Yan Zhu, Taesung Park, Phillip Isola, Alexei A. Efros.
+
+
+    Args:
+        real_images: A batch of images from domain X, a `Tensor` of shape
+            [batch_size, height, width, channels].
+        generated_images: A batch of generated images made to look like they
+            came from domain X, a `Tensor` of shape
+            [batch_size, height, width, channels].
+
+    Returns:
+        The cycle consistency loss.
+    """
+    return tf.reduce_mean(tf.abs(real_images - generated_images))
+
+
+def lsgan_loss_generator(prob_fake_is_real):
+    """Computes the LS-GAN loss as minimized by the generator.
+
+    Rather than compute the negative loglikelihood, a least-squares loss is
+    used to optimize the discriminators as per Equation 2 in:
+        Least Squares Generative Adversarial Networks
+        Xudong Mao, Qing Li, Haoran Xie, Raymond Y.K. Lau, Zhen Wang, and
+        Stephen Paul Smolley.
+        https://arxiv.org/pdf/1611.04076.pdf
+
+    Args:
+        prob_fake_is_real: The discriminator's estimate that generated images
+            made to look like real images are real.
+
+    Returns:
+        The total LS-GAN loss.
+    """
+    return tf.reduce_mean(tf.squared_difference(prob_fake_is_real, 1))
+
+
+def lsgan_loss_discriminator(prob_real_is_real, prob_fake_is_real):
+    """Computes the LS-GAN loss as minimized by the discriminator.
+
+    Rather than compute the negative loglikelihood, a least-squares loss is
+    used to optimize the discriminators as per Equation 2 in:
+        Least Squares Generative Adversarial Networks
+        Xudong Mao, Qing Li, Haoran Xie, Raymond Y.K. Lau, Zhen Wang, and
+        Stephen Paul Smolley.
+        https://arxiv.org/pdf/1611.04076.pdf
+
+    Args:
+        prob_real_is_real: The discriminator's estimate that images actually
+            drawn from the real domain are in fact real.
+        prob_fake_is_real: The discriminator's estimate that generated images
+            made to look like real images are real.
+
+    Returns:
+        The total LS-GAN loss.
+    """
+    return (tf.reduce_mean(tf.squared_difference(prob_real_is_real, 1)) +
+            tf.reduce_mean(tf.squared_difference(prob_fake_is_real, 0))) * 0.5
